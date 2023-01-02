bd8e37362d2ad6832711afb6cbd73c45a6a99619
Distractic <distractic@outlook.fr>
2023-01-02 17:44:13 +0100

    doc: Update documentation sentence

--- src/main/kotlin/com/github/rushyverse/api/position/SphereArea.kt
+++ src/main/kotlin/com/github/rushyverse/api/position/SphereArea.kt
@@ -5,26 +5,26 @@ import net.minestom.server.entity.Entity
 import net.minestom.server.instance.Instance
 
 /**
- * An area defined by a radius.
- * @param E The type of entity.
- * @property entityClass The class of the entity.
- * @property instance The instance where is located the area.
- * @property center The center of the radius.
- * @property radius The radius.
+ * An area defined by a sphere shape.
+ * @param E Type of entity.
+ * @property entityClass Class of the entity.
+ * @property instance Instance where is located the area.
+ * @property position Position of the sphere.
+ * @property radius Radius.
  */
 public class SphereArea<E : Entity>(
     public val entityClass: Class<E>,
     public var instance: Instance,
-    public var center: Pos,
+    public var position: Pos,
     radius: Double
 ) : AbstractArea<E>() {
 
     public companion object {
         public inline operator fun <reified E : Entity> invoke(
             instance: Instance,
-            center: Pos,
+            position: Pos,
             radius: Double
-        ): SphereArea<E> = SphereArea(E::class.java, instance, center, radius)
+        ): SphereArea<E> = SphereArea(E::class.java, instance, position, radius)
     }
 
     public var radius: Double = radius
@@ -46,6 +46,6 @@ public class SphereArea<E : Entity>(
     }
 
     override fun update(): Pair<Collection<E>, Collection<E>> {
-        return update(instance.getNearbyEntities(center, radius).asSequence().filterIsInstance(entityClass).toSet())
+        return update(instance.getNearbyEntities(position, radius).asSequence().filterIsInstance(entityClass).toSet())
     }
 }
