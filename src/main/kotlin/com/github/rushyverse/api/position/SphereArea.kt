329af876895fa22757f98f2723ff3f98d9b7cd44
Distractic <distractic@outlook.fr>
2023-01-02 16:26:40 +0100

    Revert "chore: Change variable name"
    This reverts commit eac71d383f63ec331c21edcf2e1f25eca13a9ce5.

--- src/main/kotlin/com/github/rushyverse/api/position/SphereArea.kt
+++ src/main/kotlin/com/github/rushyverse/api/position/SphereArea.kt
@@ -9,31 +9,33 @@ import net.minestom.server.instance.Instance
  * @param E The type of entity.
  * @property entityClass The class of the entity.
  * @property instance The instance where is located the area.
- * @property center The center of the area.
- * @property range The range.
+ * @property center The center of the radius.
+ * @property radius The radius.
  */
 public class SphereArea<E : Entity>(
     public val entityClass: Class<E>,
     public var instance: Instance,
     public var center: Pos,
-    range: Double
+    radius: Double
 ) : AbstractArea<E>() {
 
     public companion object {
         public inline operator fun <reified E : Entity> invoke(
             instance: Instance,
             position: Pos,
-            range: Double
-        ): SphereArea<E> = SphereArea(E::class.java, instance, position, range)
+            radius: Double
+        ): SphereArea<E> {
+            return SphereArea(E::class.java, instance, position, radius)
+        }
     }
 
-    public var range: Double = range
+    public var radius: Double = radius
         set(value) {
             require(value >= 0.0) { "Radius must be greater than or equal to 0.0" }
             field = value
         }
 
     override fun update(): Pair<Collection<E>, Collection<E>> {
-        return update(instance.getNearbyEntities(center, range).asSequence().filterIsInstance(entityClass).toSet())
+        return update(instance.getNearbyEntities(center, radius).asSequence().filterIsInstance(entityClass).toSet())
     }
 }
