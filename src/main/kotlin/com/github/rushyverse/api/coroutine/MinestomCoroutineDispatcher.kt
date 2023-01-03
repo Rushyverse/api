1cf7e49ff3674cab4289729de613bb77dbfb895e
Distractic <46402441+Distractic@users.noreply.github.com>
2023-01-03 22:27:42 +0100

    doc: Add missing documentation for coroutine dispatcher (#15)

--- src/main/kotlin/com/github/rushyverse/api/coroutine/MinestomCoroutineDispatcher.kt
+++ src/main/kotlin/com/github/rushyverse/api/coroutine/MinestomCoroutineDispatcher.kt
@@ -7,6 +7,12 @@ import net.minestom.server.ServerProcess
 import net.minestom.server.timer.ExecutionType
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * A coroutine dispatcher that uses the Minestom server thread.
+ * @property serverProcess The server process to use.
+ * @property type Determine if the execution should be [synchronous][ExecutionType.SYNC] or [asynchronous][ExecutionType.ASYNC].
+ * @property scope Coroutine scope using the dispatcher and a [SupervisorJob].
+ */
 public open class MinestomCoroutineDispatcher(
     private val serverProcess: ServerProcess,
     public val type: ExecutionType
