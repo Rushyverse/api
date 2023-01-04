bee4e757c41e13ab87cd973ad2f649b9927e7581
Distractic <distractic@outlook.fr>
2023-01-04 22:43:22 +0100

    tests: Begin tests about npc

---
+++ src/test/kotlin/com/github/rushyverse/api/entity/NPCEntityTest.kt
@@ -0,0 +1,61 @@
+package com.github.rushyverse.api.entity
+
+import com.github.rushyverse.api.position.IAreaLocatable
+import io.mockk.*
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.entity.EntityType
+import net.minestom.server.entity.Player
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class NPCEntityTest {
+
+    @Nested
+    inner class LookNearbyPlayer {
+
+        @Test
+        fun `should throw exception if area is null`() {
+            val npc = NPCEntity(EntityType.PLAYER, areaTrigger = null)
+            assertThrows<IllegalStateException> { npc.lookNearbyPlayer() }
+        }
+
+        @Test
+        fun `should keep the position if no player is near`() {
+            val area = mockk<IAreaLocatable<Player>>() {
+                every { entitiesInArea } returns emptySet()
+            }
+            val npc = NPCEntity(EntityType.PLAYER, area)
+            val expectedPos = npc.position
+            println(expectedPos)
+
+            npc.lookNearbyPlayer()
+            assertEquals(expectedPos, npc.position)
+        }
+
+        @Test
+        fun `should look at the player if there is one`() {
+            val player = mockk<Player>() {
+                every { position } returns Pos(10.0, 20.0, 30.0)
+                every { eyeHeight } returns 1.0
+            }
+
+            val area = mockk<IAreaLocatable<Player>>() {
+                every { entitiesInArea } returns setOf(player)
+            }
+            val npc = NPCEntity(EntityType.PLAYER, area)
+
+            val slotPlayer = slot<Player>()
+            val npcSpyk = spyk(npc) {
+                justRun { lookAt(capture(slotPlayer)) }
+            }
+
+            npc.lookNearbyPlayer()
+
+            verify(exactly = 1) { npcSpyk.lookAt(any<Player>()) }
+            assertEquals(player, slotPlayer.captured)
+        }
+
+    }
+}
