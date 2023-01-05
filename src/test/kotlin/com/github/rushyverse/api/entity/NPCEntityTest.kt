ae6033c35e2cc56dc5866b9c4256a3fea1ae0282
Distractic <distractic@outlook.fr>
2023-01-05 07:18:39 +0100

    tests: Add test cases for NPC
    Add test cases and prepare other tests

--- src/test/kotlin/com/github/rushyverse/api/entity/NPCEntityTest.kt
+++ src/test/kotlin/com/github/rushyverse/api/entity/NPCEntityTest.kt
@@ -2,8 +2,12 @@ package com.github.rushyverse.api.entity
 
 import com.github.rushyverse.api.position.IAreaLocatable
 import io.mockk.*
+import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.EntityType
 import net.minestom.server.entity.Player
+import net.minestom.server.event.player.PlayerEntityInteractEvent
+import net.minestom.testing.Env
+import net.minestom.testing.EnvTest
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
@@ -43,15 +47,140 @@ class NPCEntityTest {
             val npc = NPCEntity(EntityType.PLAYER, area)
 
             val slotPlayer = slot<Player>()
-            val npcSpyk = spyk(npc) {
+            val npcSpy = spyk(npc) {
                 justRun { lookAt(capture(slotPlayer)) }
             }
 
-            npcSpyk.lookNearbyPlayer()
+            npcSpy.lookNearbyPlayer()
 
-            verify(exactly = 1) { npcSpyk.lookAt(any<Player>()) }
+            verify(exactly = 1) { npcSpy.lookAt(any<Player>()) }
             assertEquals(player, slotPlayer.captured)
         }
 
+    }
+
+    @Nested
+    inner class OnEnterArea {
+
+        @Test
+        fun `should do nothing`() {
+            val npc = NPCEntity(EntityType.PLAYER)
+            val player = mockk<Player>()
+            npc.onEnterArea(player)
+        }
+
+    }
+
+    @Nested
+    inner class OnLeaveArea {
+
+        @Test
+        fun `should do nothing`() {
+            val npc = NPCEntity(EntityType.PLAYER)
+            val player = mockk<Player>()
+            npc.onLeaveArea(player)
+        }
+
+    }
+
+    @Nested
+    inner class OnInteract {
+
+        @Test
+        fun `should do nothing`() {
+            val npc = NPCEntity(EntityType.PLAYER)
+            val event = mockk<PlayerEntityInteractEvent>()
+            npc.onInteract(event)
+        }
+
+    }
+
+    @Nested
+    @EnvTest
+    inner class Update {
+
+        @Test
+        fun `should update the area entities`(env: Env) {
+            val pos = Pos(0.0, 0.0, 0.0)
+            val flatInstance = env.createFlatInstance()
+            val area = mockk<IAreaLocatable<Player>>() {
+                justRun { position = any() }
+                justRun { instance = any() }
+                every { updateEntitiesInArea() } returns Pair(emptySet(), emptySet())
+            }
+            val npc = NPCEntity(EntityType.PLAYER, area)
+            npc.setInstance(flatInstance, pos)
+            npc.update(0)
+
+            verify(exactly = 1) { area.instance = flatInstance }
+            verify(exactly = 1) { area.position = pos }
+            verify(exactly = 1) { area.updateEntitiesInArea() }
+        }
+
+        @Nested
+        inner class OnEnterArea {
+
+            @Test
+            fun `should trigger enter area if new player is in area`(env: Env) {
+                val pos = Pos(0.0, 0.0, 0.0)
+                val flatInstance = env.createFlatInstance()
+                val player = mockk<Player>()
+                val area = mockk<IAreaLocatable<Player>>() {
+                    justRun { position = any() }
+                    justRun { instance = any() }
+                    every { updateEntitiesInArea() } returns Pair(setOf(player), emptySet())
+                }
+                val npc = NPCEntity(EntityType.PLAYER, area)
+                npc.setInstance(flatInstance, pos)
+                val npcSpy = spyk(npc) {
+                    justRun { onEnterArea(any()) }
+                }
+                npcSpy.update(0)
+
+                verify(exactly = 1) { npcSpy.onEnterArea(player) }
+            }
+
+            @Test
+            fun `should not trigger enter area if the player is always in the area`(env: Env) {
+                TODO()
+            }
+
+        }
+
+        @Nested
+        inner class OnLeaveArea {
+
+            @Test
+            fun `should trigger leave area if player is not in area anymore`(env: Env) {
+                val pos = Pos(0.0, 0.0, 0.0)
+                val flatInstance = env.createFlatInstance()
+                val player = mockk<Player>()
+                val area = mockk<IAreaLocatable<Player>>() {
+                    justRun { position = any() }
+                    justRun { instance = any() }
+                    every { updateEntitiesInArea() } returns Pair(emptySet(), setOf(player))
+                }
+
+                val npc = NPCEntity(EntityType.PLAYER, area)
+                npc.setInstance(flatInstance, pos)
+                val npcSpy = spyk(npc) {
+                    justRun { onLeaveArea(any()) }
+                }
+                npcSpy.update(0)
+
+                verify(exactly = 1) { npcSpy.onLeaveArea(player) }
+            }
+
+            @Test
+            fun `should not leave enter area if the player is always out of the area`(env: Env) {
+                TODO()
+            }
+
+        }
+
+        @Test
+        fun `should trigger enter and leave area if players is in and out of area`(env: Env) {
+            TODO()
+        }
     }
 }
