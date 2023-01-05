77ed712e33d18b100a76bec1195554785deb1ffe
Distractic <distractic@outlook.fr>
2023-01-05 19:10:18 +0100

    feat: Add extension function for components

--- src/test/kotlin/com/github/rushyverse/api/extension/ComponentExtTest.kt
+++ src/test/kotlin/com/github/rushyverse/api/extension/ComponentExtTest.kt
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.event.ClickEvent
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.TextDecoration
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -11,6 +12,199 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ComponentExtTest {
+
+    @Nested
+    inner class ChangeDecoration {
+
+        @Nested
+        inner class Bold {
+
+            @Test
+            fun `should add bold`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.BOLD, TextDecoration.State.TRUE),
+                    Component.text("Hello").withBold()
+                )
+            }
+
+            @Test
+            fun `should remove bold`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.BOLD, TextDecoration.State.FALSE),
+                    Component.text("Hello").withoutBold()
+                )
+            }
+
+            @Test
+            fun `should undefine bold`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.BOLD, TextDecoration.State.NOT_SET),
+                    Component.text("Hello").undefineBold()
+                )
+            }
+
+        }
+
+        @Nested
+        inner class Italic {
+
+            @Test
+            fun `should add italic`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.ITALIC, TextDecoration.State.TRUE),
+                    Component.text("Hello").withItalic()
+                )
+            }
+
+            @Test
+            fun `should remove italic`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.ITALIC, TextDecoration.State.FALSE),
+                    Component.text("Hello").withoutItalic()
+                )
+            }
+
+            @Test
+            fun `should undefine italic`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.ITALIC, TextDecoration.State.NOT_SET),
+                    Component.text("Hello").undefineItalic()
+                )
+            }
+
+        }
+
+        @Nested
+        inner class Underlined {
+
+            @Test
+            fun `should add underlined`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.UNDERLINED, TextDecoration.State.TRUE),
+                    Component.text("Hello").withUnderlined()
+                )
+            }
+
+            @Test
+            fun `should remove underlined`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.UNDERLINED, TextDecoration.State.FALSE),
+                    Component.text("Hello").withoutUnderlined()
+                )
+            }
+
+            @Test
+            fun `should undefine underlined`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.UNDERLINED, TextDecoration.State.NOT_SET),
+                    Component.text("Hello").undefineUnderlined()
+                )
+            }
+
+        }
+
+        @Nested
+        inner class Strikethrough {
+
+            @Test
+            fun `should add strikethrough`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.STRIKETHROUGH, TextDecoration.State.TRUE),
+                    Component.text("Hello").withStrikethrough()
+                )
+            }
+
+            @Test
+            fun `should remove strikethrough`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.STRIKETHROUGH, TextDecoration.State.FALSE),
+                    Component.text("Hello").withoutStrikethrough()
+                )
+            }
+
+            @Test
+            fun `should undefine strikethrough`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.STRIKETHROUGH, TextDecoration.State.NOT_SET),
+                    Component.text("Hello").undefineStrikethrough()
+                )
+            }
+
+        }
+
+        @Nested
+        inner class Obfuscated {
+
+            @Test
+            fun `should add obfuscated`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.OBFUSCATED, TextDecoration.State.TRUE),
+                    Component.text("Hello").withObfuscated()
+                )
+            }
+
+            @Test
+            fun `should remove obfuscated`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.OBFUSCATED, TextDecoration.State.FALSE),
+                    Component.text("Hello").withoutObfuscated()
+                )
+            }
+
+            @Test
+            fun `should undefine obfuscated`() {
+                assertEquals(
+                    Component.text("Hello").decoration(TextDecoration.OBFUSCATED, TextDecoration.State.NOT_SET),
+                    Component.text("Hello").undefineObfuscated()
+                )
+            }
+
+        }
+
+        @Nested
+        inner class Decorations {
+
+            @Test
+            fun `should add all decorations`() {
+                assertEquals(
+                    Component.text("Hello")
+                        .decoration(TextDecoration.BOLD, TextDecoration.State.TRUE)
+                        .decoration(TextDecoration.ITALIC, TextDecoration.State.TRUE)
+                        .decoration(TextDecoration.UNDERLINED, TextDecoration.State.TRUE)
+                        .decoration(TextDecoration.STRIKETHROUGH, TextDecoration.State.TRUE)
+                        .decoration(TextDecoration.OBFUSCATED, TextDecoration.State.TRUE),
+                    Component.text("Hello").withDecorations()
+                )
+            }
+
+            @Test
+            fun `should remove all decorations`() {
+                assertEquals(
+                    Component.text("Hello")
+                        .decoration(TextDecoration.BOLD, TextDecoration.State.FALSE)
+                        .decoration(TextDecoration.ITALIC, TextDecoration.State.FALSE)
+                        .decoration(TextDecoration.UNDERLINED, TextDecoration.State.FALSE)
+                        .decoration(TextDecoration.STRIKETHROUGH, TextDecoration.State.FALSE)
+                        .decoration(TextDecoration.OBFUSCATED, TextDecoration.State.FALSE),
+                    Component.text("Hello").withoutDecorations()
+                )
+            }
+
+            @Test
+            fun `should undefine all decorations`() {
+                assertEquals(
+                    Component.text("Hello")
+                        .decoration(TextDecoration.BOLD, TextDecoration.State.NOT_SET)
+                        .decoration(TextDecoration.ITALIC, TextDecoration.State.NOT_SET)
+                        .decoration(TextDecoration.UNDERLINED, TextDecoration.State.NOT_SET)
+                        .decoration(TextDecoration.STRIKETHROUGH, TextDecoration.State.NOT_SET)
+                        .decoration(TextDecoration.OBFUSCATED, TextDecoration.State.NOT_SET),
+                    Component.text("Hello").undefineDecorations()
+                )
+            }
+
+        }
+    }
 
     @Nested
     inner class StringToComponent {
@@ -30,9 +224,12 @@ class ComponentExtTest {
 
         @Test
         fun `should separate text and url`() {
-            val component = "Hello https://www.youtube.com &#3b81f1".toComponent(extractUrls = true, extractColors = false)
+            val component =
+                "Hello https://www.youtube.com &#3b81f1".toComponent(extractUrls = true, extractColors = false)
             val expected = Component.text("Hello ")
-                .append(Component.text("https://www.youtube.com").clickEvent(ClickEvent.openUrl("https://www.youtube.com")))
+                .append(
+                    Component.text("https://www.youtube.com").clickEvent(ClickEvent.openUrl("https://www.youtube.com"))
+                )
                 .append(Component.text(" &#3b81f1"))
             assertEquals(expected, component)
         }
@@ -91,7 +288,9 @@ class ComponentExtTest {
 
         @Test
         fun `should return text with url`() {
-            val text = Component.text("Hello ").append(Component.text("https://www.youtube.com").clickEvent(ClickEvent.openUrl("https://www.youtube.com"))).toText()
+            val text = Component.text("Hello ").append(
+                Component.text("https://www.youtube.com").clickEvent(ClickEvent.openUrl("https://www.youtube.com"))
+            ).toText()
             assertEquals("Hello https://www.youtube.com", text)
         }
     }
