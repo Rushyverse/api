fb20fb8c4108bd54a5cb143b9b960e805cf104c1
Distractic <distractic@outlook.fr>
2023-02-07 19:09:20 +0100

    tests: Add tests for inventory condition suspend

--- src/test/kotlin/com/github/rushyverse/api/item/InventoryConditionSuspendTest.kt
+++ src/test/kotlin/com/github/rushyverse/api/item/InventoryConditionSuspendTest.kt
@@ -1,7 +1,18 @@
 package com.github.rushyverse.api.item
 
+import com.github.rushyverse.api.utils.assertCoroutineContextFromScope
+import com.github.rushyverse.api.utils.randomString
+import io.mockk.mockk
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.yield
+import net.minestom.server.entity.Player
+import net.minestom.server.inventory.click.ClickType
+import net.minestom.server.inventory.condition.InventoryConditionResult
 import org.junit.jupiter.api.Nested
+import kotlin.coroutines.coroutineContext
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class InventoryConditionSuspendTest {
 
@@ -10,17 +21,46 @@ class InventoryConditionSuspendTest {
 
         @Test
         fun `should use params sent to the native inventory`() {
-            TODO()
+            val expectedPlayer = mockk<Player>(randomString())
+            val expectedSlot = 420
+            val expectedClickType = mockk<ClickType>(randomString())
+            val expectedInventoryConditionResult = mockk<InventoryConditionResult>(randomString())
+
+            val inventoryConditionSuspend = InventoryConditionSuspend { player, clickedSlot, clickType, result ->
+                assertEquals(expectedPlayer, player)
+                assertEquals(expectedSlot, clickedSlot)
+                assertEquals(expectedClickType, clickType)
+                assertEquals(expectedInventoryConditionResult, result)
+            }
+
+            val coroutineScope = CoroutineScope(Dispatchers.Default)
+            val inventoryCondition = inventoryConditionSuspend.asNative(coroutineScope)
+            inventoryCondition.accept(expectedPlayer, expectedSlot, expectedClickType, expectedInventoryConditionResult)
         }
 
         @Test
-        fun `should receive the result from the handle`() {
-            TODO()
+        fun `should stay in current thread before suspend point`() {
+            val thread = Thread.currentThread().id
+            val inventoryConditionSuspend = InventoryConditionSuspend { _, _, _, _ ->
+                assertEquals(thread, Thread.currentThread().id)
+            }
+
+            val coroutineScope = CoroutineScope(Dispatchers.Default)
+            val inventoryCondition = inventoryConditionSuspend.asNative(coroutineScope)
+            inventoryCondition.accept(mockk(), 0, mockk(), mockk())
         }
 
         @Test
-        fun `should ignore result from the handle after suspending point`() {
-            TODO()
+        fun `should change thread context after suspend point`() {
+            val coroutineScope = CoroutineScope(Dispatchers.Default)
+
+            val inventoryConditionSuspend = InventoryConditionSuspend { _, _, _, _ ->
+                yield()
+                assertCoroutineContextFromScope(coroutineScope, coroutineContext)
+            }
+
+            val inventoryCondition = inventoryConditionSuspend.asNative(coroutineScope)
+            inventoryCondition.accept(mockk(), 0, mockk(), mockk())
         }
 
     }
