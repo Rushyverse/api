8af8f17092ed6fe74eb50b57c1477351ea44805b
Distractic <distractic@outlook.fr>
2023-02-07 18:10:56 +0100

    tests: Prepare tests for inv condition suspend

---
+++ src/test/kotlin/com/github/rushyverse/api/item/InventoryConditionSuspendTest.kt
@@ -0,0 +1,27 @@
+package com.github.rushyverse.api.item
+
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+
+class InventoryConditionSuspendTest {
+
+    @Nested
+    inner class AsNative {
+
+        @Test
+        fun `should use params sent to the native inventory`() {
+            TODO()
+        }
+
+        @Test
+        fun `should receive the result from the handle`() {
+            TODO()
+        }
+
+        @Test
+        fun `should ignore result from the handle after suspending point`() {
+            TODO()
+        }
+
+    }
+}
