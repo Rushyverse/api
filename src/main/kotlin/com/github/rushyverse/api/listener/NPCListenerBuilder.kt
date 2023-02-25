9980fdf65fdc133c6f5c4821956f67b3e08bc4b5
Distractic <distractic@outlook.fr>
2023-02-25 22:21:31 +0100

    doc: Add documentation for node builder

--- src/main/kotlin/com/github/rushyverse/api/listener/NPCListenerBuilder.kt
+++ src/main/kotlin/com/github/rushyverse/api/listener/NPCListenerBuilder.kt
@@ -7,17 +7,29 @@ import net.minestom.server.event.EventListener
 import net.minestom.server.event.EventNode
 import net.minestom.server.event.player.PlayerEntityInteractEvent
 
+/**
+ * Builder to create an event node to listen to NPC events.
+ */
 public object NPCListenerBuilder {
 
+    /**
+     * Create an event node to listen to NPC events.
+     * @return A new event node.
+     */
     public fun createEventNode(): EventNode<Event> {
         return EventNode.all("npc").apply {
             addInteractListener(this)
         }
     }
 
+    /**
+     * Add a listener to the node to listen when a player interacts with an NPC.
+     * @param node Event node to add the listener.
+     */
     private fun addInteractListener(node: EventNode<Event>) {
-        node.addListener(EventListener.builder(PlayerEntityInteractEvent::class.java)
-            .filter { it.hand == Player.Hand.MAIN && it.target is NPCEntity }
+        node.addListener(
+            EventListener.builder(PlayerEntityInteractEvent::class.java)
+            .filter { it.target is NPCEntity && it.hand == Player.Hand.MAIN }
             .handler {
                 val npc = it.target as NPCEntity
                 npc.onInteract(it)
