7414a912abcf6ab6b66ff09d2d2ee4555fd591d7
Distractic <distractic@outlook.fr>
2023-02-26 17:22:54 +0100

    tests: Rename class

--- src/test/kotlin/com/github/rushyverse/api/configuration/VelocityConfigurationTest.kt
+++ src/test/kotlin/com/github/rushyverse/api/configuration/VelocityConfigurationTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.assertEquals
 
-class VelocityConfigurationConfigurationTest {
+class VelocityConfigurationTest {
 
     @Nested
     inner class Serialize {
