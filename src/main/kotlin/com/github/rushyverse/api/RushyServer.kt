eca8a292e2fbe5bfa60b7fb100c564f5402cd620
Quentin <nelsonsims@hotmail.fr>
2023-05-04 10:27:19 +0200

    fix: Adapt the class for a JavaPlugin

--- src/main/kotlin/com/github/rushyverse/api/RushyServer.kt
+++ src/main/kotlin/com/github/rushyverse/api/RushyServer.kt
@@ -1,25 +1,13 @@
 package com.github.rushyverse.api
 
-import com.github.rushyverse.api.command.GamemodeCommand
-import com.github.rushyverse.api.command.GiveCommand
-import com.github.rushyverse.api.command.KickCommand
-import com.github.rushyverse.api.command.StopCommand
 import com.github.rushyverse.api.configuration.*
 import com.github.rushyverse.api.translation.ResourceBundleTranslationsProvider
 import com.github.rushyverse.api.translation.SupportedLanguage
 import com.github.rushyverse.api.translation.TranslationsProvider
 import com.github.rushyverse.api.translation.registerResourceBundleForSupportedLocales
-import com.github.rushyverse.api.utils.workingDirectory
 import mu.KLogger
 import mu.KotlinLogging
-import net.minestom.server.MinecraftServer
-import net.minestom.server.command.CommandManager
-import net.minestom.server.extras.MojangAuth
-import net.minestom.server.extras.bungee.BungeeCordProxy
-import net.minestom.server.extras.velocity.VelocityProxy
-import net.minestom.server.instance.AnvilLoader
-import net.minestom.server.instance.InstanceContainer
-import java.io.File
+import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
 import kotlin.reflect.KClass
 
@@ -28,7 +16,7 @@ public val logger: KLogger = KotlinLogging.logger { }
 /**
  * Abstract implementation of Minecraft server.
  */
-public abstract class RushyServer {
+public abstract class RushyServer : JavaPlugin() {
 
     public companion object API {
         /**
@@ -37,9 +25,9 @@ public abstract class RushyServer {
         public const val BUNDLE_API: String = "api"
 
         /**
-         * Register the API commands in the [CommandManager].
+         * Register the API commands.
          */
-        public fun registerCommands( ) {
+        public fun registerCommands() {
             // New commands here
         }
     }
@@ -56,38 +44,18 @@ public abstract class RushyServer {
      */
     protected suspend inline fun <reified T : IConfiguration> start(
         configurationPath: String? = null,
-        init: T.(InstanceContainer) -> Unit
+        init: (T) -> Unit
     ) {
         logger.info { "Loading configuration from $configurationPath" }
         val config = loadConfiguration<T>(configurationPath)
         logger.info { "Configuration loaded" }
 
-        val minecraftServer = MinecraftServer.init()
-        val instanceManager = MinecraftServer.getInstanceManager()
-        val instanceContainer = instanceManager.createInstanceContainer()
         val serverConfig = config.server
-        loadWorld(serverConfig.world, instanceContainer)
 
         applyVelocityConfiguration(serverConfig.velocity)
         applyBungeeCordConfiguration(serverConfig.bungeeCord)
 
-        applyOnlineMode(serverConfig.onlineMode)
-
-        init(config, instanceContainer)
-
-        minecraftServer.start("0.0.0.0", serverConfig.port)
-    }
-
-    /**
-     * Enable the online mode if [enabled] is true.
-     * @param enabled If the online mode should be enabled.
-     */
-    protected open suspend fun applyOnlineMode(enabled: Boolean) {
-        if (enabled) {
-            logger.info { "Enabling Online mode" }
-            MojangAuth.init()
-            logger.info { "Online mode enabled" }
-        }
+        init(config)
     }
 
     /**
@@ -97,7 +65,7 @@ public abstract class RushyServer {
     protected open suspend fun applyVelocityConfiguration(velocity: IVelocityConfiguration) {
         if (velocity.enabled) {
             logger.info { "Enabling Velocity support" }
-            VelocityProxy.enable(velocity.secret)
+            // TODO: Velocity configuration here
             logger.info { "Velocity support enabled" }
         }
     }
@@ -109,27 +77,9 @@ public abstract class RushyServer {
     protected open suspend fun applyBungeeCordConfiguration(bungeeCord: IBungeeCordConfiguration) {
         if (bungeeCord.enabled) {
             logger.info { "Enabling BungeeCord support" }
-            BungeeCordProxy.enable()
-            BungeeCordProxy.setBungeeGuardTokens(bungeeCord.secrets)
+            // TODO: BungeeCord configuration here
             logger.info { "BungeeCord support enabled" }
         }
-    }
-
-    /**
-     * With the [worldFolder], retrieve the file of the world and load it in the [instanceContainer].
-     * @param worldFolder World folder.
-     * @param instanceContainer Instance container of the server.
-     */
-    protected open suspend fun loadWorld(worldFolder: String, instanceContainer: InstanceContainer) {
-        val anvilWorld = File(workingDirectory, worldFolder)
-        if (!anvilWorld.isDirectory) {
-            throw FileSystemException(
-                anvilWorld,
-                null,
-                "World ${anvilWorld.absolutePath} does not exist or is not a directory"
-            )
-        }
-        instanceContainer.chunkLoader = AnvilLoader(anvilWorld.toPath())
     }
 
     /**
