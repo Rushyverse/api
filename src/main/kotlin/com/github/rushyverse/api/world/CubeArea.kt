08022b24c15dacc839c40829346f23b84637dcce
Distractic <distractic@outlook.fr>
2023-07-27 13:46:26 +0200

    fix(area): Refactor CubeArea class constructor and initializer block
    Simplified CubeArea class constructor declaration and updated the initializer block to enforce the added requirement that both location parameters should be in the same world. This change ensures that the locations passed are consistent, thus avoiding potential issues in the future.

--- src/main/kotlin/com/github/rushyverse/api/world/CubeArea.kt
+++ src/main/kotlin/com/github/rushyverse/api/world/CubeArea.kt
@@ -10,10 +10,7 @@ import org.bukkit.Location
  * @property max Maximum position.
  * @property location Center position of the cube.
  */
-public class CubeArea(
-    loc1: Location,
-    loc2: Location
-) {
+public class CubeArea(loc1: Location, loc2: Location) {
 
     public var location: Location
         get() = max.centerRelative(min)
@@ -31,10 +28,14 @@ public class CubeArea(
         private set
 
     init {
+        val world1 = loc1.world
+        val world2 = loc2.world
+        require(world1 === world2) { "Locations must be in the same world" }
+
         val (x1, x2) = minMaxOf(loc1.x, loc2.x)
         val (y1, y2) = minMaxOf(loc1.y, loc2.y)
         val (z1, z2) = minMaxOf(loc1.z, loc2.z)
-        this.min = Location(loc1.world, x1, y1, z1)
-        this.max = Location(loc2.world, x2, y2, z2)
+        this.min = Location(world1, x1, y1, z1)
+        this.max = Location(world2, x2, y2, z2)
     }
 }
