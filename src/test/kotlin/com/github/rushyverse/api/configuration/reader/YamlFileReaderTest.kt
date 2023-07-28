a0e51e8c9fe1dd175ccefa5a53f38495c18a9414
Distractic <distractic@outlook.fr>
2023-07-29 00:26:24 +0200

    tests: Add debug for CI

--- src/test/kotlin/com/github/rushyverse/api/configuration/reader/YamlFileReaderTest.kt
+++ src/test/kotlin/com/github/rushyverse/api/configuration/reader/YamlFileReaderTest.kt
@@ -101,9 +101,11 @@ class YamlFileReaderTest {
         every { plugin.dataFolder } returns tempDir
 
         val expectedValue = TestValue(value)
+        println("Load config : ${load(configFile)}")
         load(configFile) shouldBe expectedValue
 
         val file = File(tempDir, configFile)
+        println("File [${file.absolutePath}] exists : ${file.exists()}")
         file.exists() shouldBe true
         file.readText() shouldBe "test: $value\r\n"
     }
