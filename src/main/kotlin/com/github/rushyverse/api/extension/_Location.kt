d7b6391ee57ef07f8267dc4866ddb1d5e4cf53ae
Distractic <distractic@outlook.fr>
2023-07-29 08:50:01 +0200

    chore: Remove unused code

--- src/main/kotlin/com/github/rushyverse/api/extension/_Location.kt
+++ src/main/kotlin/com/github/rushyverse/api/extension/_Location.kt
@@ -4,56 +4,6 @@ import org.bukkit.Location
 import org.bukkit.World
 
 /**
- * Get the [Location.world] value from the location.
- * @receiver Location.
- * @return The world where is the location.
- */
-public operator fun Location.component1(): World = world
-
-/**
- * Get the [Location.x] value from the location.
- * @receiver Location.
- * @return Value of the coordinate x.
- */
-public operator fun Location.component2(): Double = x
-
-/**
- * Get the [Location.y] value from the location.
- * @receiver Location.
- * @return Value of the coordinate y.
- */
-public operator fun Location.component3(): Double = y
-
-/**
- * Get the [Location.z] value from the location.
- * @receiver Location.
- * @return Value of the coordinate z.
- */
-public operator fun Location.component4(): Double = z
-
-/**
- * Get the [Location.yaw] value from the location.
- * @receiver Location.
- * @return Value of the coordinate yaw.
- */
-public operator fun Location.component5(): Float = yaw
-
-/**
- * Get the [Location.pitch] value from the location.
- * @receiver Location.
- * @return Value of the coordinate pitch.
- */
-public operator fun Location.component6(): Float = pitch
-
-/**
- * Create a new location with the coordinate center to the current block.
- * Define the x and z properties to the block location + 0.5.
- * @receiver Location.
- * @return New location corresponding to the current location center on the block.
- */
-public fun Location.center(): Location = copy(x = blockX + 0.5, z = blockZ + 0.5)
-
-/**
  * Define the position (x, y, z, pitch, yaw) and the world with the same value as the location in parameter.
  * @receiver Location that will have its values modified
  * @param location Location where values will be retrieved.
@@ -87,6 +37,13 @@ public fun Location.copy(
     pitch: Float = this.pitch,
 ): Location = Location(world, x, y, z, yaw, pitch)
 
+/**
+ * Divides each coordinate of the current location by the given value.
+ *
+ * @receiver The current location.
+ * @param value The value to divide each coordinate by.
+ * @return A new location with the divided coordinates.
+ */
 public fun Location.divide(value: Number): Location {
     val toDouble = value.toDouble()
     return copy(x = x / toDouble, y = y / toDouble, z = z / toDouble)
