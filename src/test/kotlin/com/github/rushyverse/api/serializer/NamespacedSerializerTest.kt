bec75f22e01393dd07fd99016b65c069a0ebac22
Distractic <distractic@outlook.fr>
2023-08-05 17:51:49 +0200

    tests(namespace): Add tests for serializer

---
+++ src/test/kotlin/com/github/rushyverse/api/serializer/NamespacedSerializerTest.kt
@@ -0,0 +1,82 @@
+package com.github.rushyverse.api.serializer
+
+import be.seeseemelk.mockbukkit.MockBukkit
+import com.github.rushyverse.api.utils.randomString
+import io.kotest.assertions.json.shouldEqualJson
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.json.Json
+import org.bukkit.NamespacedKey
+import org.bukkit.enchantments.Enchantment
+import org.junit.jupiter.api.Nested
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class NamespacedSerializerTest {
+
+    @BeforeTest
+    fun onBefore() {
+        MockBukkit.mock()
+        Enchantment.values().isEmpty() shouldBe false
+    }
+
+    @AfterTest
+    fun onAfter() {
+        MockBukkit.unmock()
+    }
+
+    @Nested
+    inner class Serialize {
+
+        @Test
+        fun `should use namespace and key`() {
+            val namespace = randomAcceptableNamespace()
+            val key = randomAcceptableNamespace()
+            Json.encodeToString(NamespacedSerializer, NamespacedKey(namespace, key)) shouldEqualJson """
+                "$namespace:$key"
+            """.trimIndent()
+        }
+    }
+
+    @Nested
+    inner class Deserialize {
+
+        @Test
+        fun `should create with minecraft namespace by default if not defined`() {
+            val key = randomAcceptableNamespace()
+            val json = """
+                "$key"
+            """.trimIndent()
+
+            Json.decodeFromString(NamespacedSerializer, json) shouldBe NamespacedKey("minecraft", key)
+        }
+
+        @Test
+        fun `should create with namespace and key`() {
+            val namespace = randomAcceptableNamespace()
+            val key = randomAcceptableNamespace()
+            val json = """
+                "$namespace:$key"
+            """.trimIndent()
+
+            Json.decodeFromString(NamespacedSerializer, json) shouldBe NamespacedKey(namespace, key)
+        }
+
+        @Test
+        fun `should create with uppercase instead of underscore`() {
+            fun decode(namespace: String?, key: String, expected: NamespacedKey) {
+                val json = """
+                    "${if (namespace != null) "$namespace:" else ""}$key"
+                """.trimIndent()
+
+                Json.decodeFromString(NamespacedSerializer, json) shouldBe expected
+            }
+            decode("test", "myKey", NamespacedKey("test", "my_key"))
+            decode("myNamespace", "myKey", NamespacedKey("my_namespace", "my_key"))
+            decode("myNamespace", "myKey", NamespacedKey("my_namespace", "my_key"))
+            decode(null, "myKey", NamespacedKey.minecraft("my_key"))
+        }
+    }
+
+    fun randomAcceptableNamespace() = randomString(('a'..'z') + ('0'..'9') + '.' + '_' + '-')
+}
