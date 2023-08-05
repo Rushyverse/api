ab9721ddbc231a4d6f59797fecad14fcde533acf
Distractic <distractic@outlook.fr>
2023-08-05 06:40:27 +0200

    fix(enum): Exception message

--- src/main/kotlin/com/github/rushyverse/api/serializer/EnumSerializer.kt
+++ src/main/kotlin/com/github/rushyverse/api/serializer/EnumSerializer.kt
@@ -46,6 +46,6 @@ public open class EnumSerializer<T : Enum<T>>(
     public fun findEnumValue(decoded: String): T {
         val name = decoded.uppercase().replace(" ", "_")
         return values.firstOrNull { it.name == name }
-            ?: throw SerializationException("Invalid enum value: $name. Valid values are: ${values.joinToString { it.name }}")
+            ?: throw SerializationException("Invalid enum value: $decoded. Valid values are: ${values.joinToString { it.name }}")
     }
 }
