b5559262d7460ffbd8e128f09f53d58f5ff65702
Distractic <distractic@outlook.fr>
2023-08-06 01:10:20 +0200

    test(itemStack): Begin test about serialization

---
+++ src/test/kotlin/com/github/rushyverse/api/serializer/ItemStackSerializerTest.kt
@@ -0,0 +1,86 @@
+package com.github.rushyverse.api.serializer
+
+import be.seeseemelk.mockbukkit.MockBukkit
+import be.seeseemelk.mockbukkit.WorldMock
+import be.seeseemelk.mockbukkit.inventory.meta.ItemMetaMock
+import com.github.rushyverse.api.utils.randomDouble
+import com.github.rushyverse.api.utils.randomEnum
+import com.github.rushyverse.api.utils.randomFloat
+import com.github.rushyverse.api.utils.randomInt
+import io.kotest.assertions.json.shouldEqualJson
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemStack
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class ItemStackSerializerTest {
+
+    @BeforeTest
+    fun onBefore() {
+        MockBukkit.mock()
+    }
+
+    @AfterTest
+    fun onAfter() {
+        MockBukkit.unmock()
+    }
+
+    @Nested
+    inner class Serialize {
+
+        @Test
+        fun `with only material`() {
+            val material = randomEnum<Material>()
+            val amount = randomInt(1, 64)
+            val itemStack = ItemStack(material, amount)
+            val json = Json.encodeToString(ItemStackSerializer, itemStack)
+            json shouldEqualJson """
+                {
+                    "material": "${material.name}",
+                    "amount": $amount,
+                    "enchantments": {}
+                }
+            """.trimIndent()
+        }
+
+        @Test
+        @Disabled // Waiting for https://github.com/MockBukkit/MockBukkit/pull/831
+        fun `with enchantments`() {
+            val itemStack = ItemStack(Material.WOODEN_AXE).apply {
+                addUnsafeEnchantment(Enchantment.ARROW_DAMAGE, 1)
+                addUnsafeEnchantment(Enchantment.ARROW_FIRE, 42)
+            }
+            val json = Json.encodeToString(ItemStackSerializer, itemStack)
+            json shouldEqualJson """
+                {
+                  "material": "WOODEN_AXE",
+                  "amount": 1,
+                  "enchantments": {
+                    "power": 1,
+                    "flame": 42
+                  },
+                  "unbreakable": null,
+                  "customMetaModel": null,
+                  "destroyableKeys": [],
+                  "placeableKeys": null,
+                  "displayName": null,
+                  "lore": null,
+                  "durability": null,
+                  "texture": null,
+                  "patterns": null,
+                  "flags": null
+                }
+            """.trimIndent()
+        }
+
+    }
+}
