c50608d3622905e634f6249c420621be24fcfc84
Distractic <distractic@outlook.fr>
2023-08-08 21:25:54 +0200

    feat: Simple and basic implementation

--- src/main/kotlin/com/github/rushyverse/api/extension/_Duration.kt
+++ src/main/kotlin/com/github/rushyverse/api/extension/_Duration.kt
@@ -1,5 +1,8 @@
 package com.github.rushyverse.api.extension
 
+import com.github.rushyverse.api.APIPlugin
+import com.github.rushyverse.api.translation.Translator
+import java.util.*
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -43,3 +46,61 @@ public val UShort.ticks: Duration get() = toShort().ticks
  * 1 tick corresponding to 50 milliseconds, 20 ticks to 1 second.
  */
 public val Short.ticks: Duration get() = (this * MILLISECOND_PER_TICK).milliseconds
+
+public fun Duration.format(
+    translator: Translator,
+    locale: Locale,
+    bundle: String = APIPlugin.BUNDLE_API,
+    separator: String = " ",
+    infiniteSymbol: String = "∞"
+): String {
+    return format(
+        formatHour = { translator.get("time.hour.short", locale, arrayOf(it), bundle) },
+        formatMinute = { translator.get("time.minute.short", locale, arrayOf(it), bundle) },
+        formatSecond = { translator.get("time.short", locale, arrayOf(it), bundle) },
+        separator = separator,
+        infiniteSymbol = infiniteSymbol
+    )
+}
+
+public inline fun Duration.format(
+    formatHour: (String) -> String,
+    formatMinute: (String) -> String,
+    formatSecond: (String) -> String,
+    separator: String = " ",
+    infiniteSymbol: String = "∞"
+): String {
+    require(!this.isNegative()) { "Number must be positive" }
+
+    val hours = this.inWholeHours
+    val minutes = this.inWholeMinutes % 60
+    val seconds = this.inWholeSeconds % 60
+
+    val hoursString: String
+    val minutesString: String
+    val secondsString: String
+
+    if(isInfinite()) {
+        hoursString = infiniteSymbol
+        minutesString = infiniteSymbol
+        secondsString = infiniteSymbol
+    } else {
+        hoursString = String.format("%02d", hours)
+        minutesString = String.format("%02d", minutes)
+        secondsString = String.format("%02d", seconds)
+    }
+
+    return buildString {
+        if (hours > 0) {
+            append(formatHour(hoursString))
+            append(separator)
+        }
+
+        if (hours > 0 || minutes > 0) {
+            append(formatMinute(minutesString))
+            append(separator)
+        }
+
+        append(formatSecond(secondsString))
+    }
+}
--- src/test/kotlin/com/github/rushyverse/api/extension/DurationExtTest.kt
+++ src/test/kotlin/com/github/rushyverse/api/extension/DurationExtTest.kt
@@ -2,10 +2,15 @@ package com.github.rushyverse.api.extension
 
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 class DurationExtTest {
@@ -110,5 +115,83 @@ class DurationExtTest {
             assertEquals(5.seconds, 100.toULong().ticks)
             assertEquals(250.milliseconds, 5.toULong().ticks)
         }
+    }
+
+    @Nested
+    inner class TimeFormat {
+
+        private val formatHour: (String) -> String = { it + "h" }
+        private val formatMinute: (String) -> String = { it + "m" }
+        private val formatSecond: (String) -> String = { it + "s" }
+
+        @Test
+        fun `should throw an exception if the duration is negative`() {
+            assertThrows<IllegalArgumentException> {
+                (-1).seconds.format(
+                    formatHour,
+                    formatMinute,
+                    formatSecond
+                )
+            }
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = ["", " ", "-"])
+        fun `should use separator between the different parts`(separator: String) {
+            val time = (1.hours + 2.minutes + 3.seconds).format(
+                formatHour,
+                formatMinute,
+                formatSecond,
+                separator = separator
+            )
+            assertEquals(
+                "01h${separator}02m${separator}03s",
+                time
+            )
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = ["∞", "inf"])
+        fun `should return infinity if the duration is infinite`(infinity: String) {
+            assertEquals(
+                "${infinity}h ${infinity}m ${infinity}s",
+                Duration.INFINITE.format(formatHour, formatMinute, formatSecond, infiniteSymbol = infinity))
+        }
+
+        @Test
+        fun `should return the correct format for 0`() {
+            assertEquals("00s", Duration.ZERO.format(formatHour, formatMinute, formatSecond))
+        }
+
+        @Test
+        fun `should return the correct format for 1 second`() {
+            assertEquals("01s", 1.seconds.format(formatHour, formatMinute, formatSecond))
+        }
+
+        @Test
+        fun `should return the correct format for 1 minute`() {
+            assertEquals("01m 00s", 1.minutes.format(formatHour, formatMinute, formatSecond))
+        }
+
+        @Test
+        fun `should return the correct format for 1 hour`() {
+            assertEquals("01h 00m 00s", 1.hours.format(formatHour, formatMinute, formatSecond))
+        }
+
+        @Test
+        fun `should return the correct format for 1 hour 1 minute`() {
+            assertEquals("01h 01m 00s", (1.hours + 1.minutes).format(formatHour, formatMinute, formatSecond))
+        }
+
+        @Test
+        fun `should return the correct format for 1 hour 1 second`() {
+            assertEquals("01h 00m 01s", (1.hours + 1.seconds).format(formatHour, formatMinute, formatSecond))
+        }
+
+        @Test
+        fun `should return the correct format for 1 minute 1 second`() {
+            assertEquals("01m 01s", (1.minutes + 1.seconds).format(formatHour, formatMinute, formatSecond))
+        }
+
     }
 }
