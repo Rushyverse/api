5b8337136d2be80953fd52c72bc2a826c9894815
Distractic <distractic@outlook.fr>
2023-12-20 13:54:05 +0100

    test: Add test about shift  constructor

--- src/test/kotlin/com/github/rushyverse/api/gui/load/ShiftInventoryLoadingAnimationTest.kt
+++ src/test/kotlin/com/github/rushyverse/api/gui/load/ShiftInventoryLoadingAnimationTest.kt
@@ -1,18 +1,25 @@
 package com.github.rushyverse.api.gui.load
 
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import java.util.*
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.nanoseconds
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class ShiftInventoryLoadingAnimationTest {
 
@@ -30,6 +37,30 @@ class ShiftInventoryLoadingAnimationTest {
                     contents[index] = itemStack
                 }
             }
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1, -2, -3, -4, -5, -6, -7, -8])
+    fun `should throw if duration is not positive`(duration: Int) {
+        shouldThrow<IllegalArgumentException> {
+            ShiftInventoryLoadingAnimation<Unit>(
+                initialize = { emptySequence() },
+                shift = 1,
+                delay = duration.milliseconds
+            )
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8])
+    fun `should not throw if duration is positive`(duration: Int) {
+        shouldNotThrow<IllegalArgumentException> {
+            ShiftInventoryLoadingAnimation<Unit>(
+                initialize = { emptySequence() },
+                shift = 1,
+                delay = duration.nanoseconds
+            )
         }
     }
 
@@ -53,12 +84,21 @@ class ShiftInventoryLoadingAnimationTest {
 
     @Test
     fun `should just initialize if shift is zero`() = runBlocking {
+        shouldJustInitializeWithoutShift(0)
+    }
+
+    @Test
+    fun `should just initialize if shift is inventory size`() = runBlocking {
+        shouldJustInitializeWithoutShift(inventory.size)
+    }
+
+    private suspend fun shouldJustInitializeWithoutShift(shift: Int) = coroutineScope {
         val delay = 10.milliseconds
         val items = Array(inventory.size) { mockk<ItemStack>() }
 
         val animation = ShiftInventoryLoadingAnimation<Unit>(
             initialize = { items.asSequence() },
-            shift = 0,
+            shift = shift,
             delay = delay
         )
 
@@ -67,5 +107,31 @@ class ShiftInventoryLoadingAnimationTest {
         job.cancelAndJoin()
 
         inventory.contents.toList() shouldContainExactly items.toList()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8])
+    fun `should shift initialized items`(shift: Int) = runBlocking {
+        val delay = 100.milliseconds
+        val items = Array(inventory.size) { mockk<ItemStack>() }
+        val itemList = items.toList()
+
+        val animation = ShiftInventoryLoadingAnimation<Unit>(
+            initialize = { items.asSequence() },
+            shift = shift,
+            delay = delay
+        )
+
+        val job = launch { animation.loading(Unit, inventory) }
+
+        delay(delay / 2)
+        repeat(5) {
+            inventory.contents.toList() shouldContainExactly itemList
+            delay(delay)
+            Collections.rotate(itemList, shift)
+            job.isActive shouldBe true
+        }
+
+        job.cancelAndJoin()
     }
 }
